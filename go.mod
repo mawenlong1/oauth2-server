8862c87b793b8afcf36e84f52634fd919921d219
mawenlong <mwljndx@163.com>
2019-04-16 22:22:51 +0800

    session

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/RichardKnop/logging v0.0.0-20181101035820-b1d5d44c82d6
 	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
 	github.com/gorilla/mux v1.6.2
+	github.com/gorilla/sessions v1.1.3
 	github.com/jinzhu/configor v1.0.0
 	github.com/jinzhu/gorm v1.9.4
 	github.com/jinzhu/now v1.0.0 // indirect
---
+++ session/service.go
@@ -0,0 +1,123 @@
+package session
+
+import (
+	"encoding/gob"
+	"errors"
+	"github.com/gorilla/sessions"
+	"net/http"
+	"oauth2-server/config"
+)
+
+//Service ...
+type Service struct {
+	sessionStore   sessions.Store
+	sessionOptions *sessions.Options
+	session        *sessions.Session
+	r              *http.Request
+	w              http.ResponseWriter
+}
+
+//UserSession ..
+type UserSession struct {
+	ClientID     string
+	Username     string
+	AccessToken  string
+	RefreshToken string
+}
+
+var (
+	//StorageSessionName ...
+	StorageSessionName = "oauth2_server_session"
+	//UserSessionKey ...
+	UserSessionKey = "oauth2_server_user"
+	//ErrSessionNotStarted ...
+	ErrSessionNotStarted = errors.New("Session not started")
+)
+
+func init() {
+	gob.Register(new(UserSession))
+}
+
+//NewService ...
+func NewService(config *config.Config, store sessions.Store) *Service {
+	return &Service{
+		sessionStore: store,
+		sessionOptions: &sessions.Options{
+			Path:     config.Session.Path,
+			MaxAge:   config.Session.MaxAge,
+			HttpOnly: config.Session.HTTPOnly,
+		},
+	}
+}
+
+//SetSessionService ...
+func (s *Service) SetSessionService(r *http.Request, w http.ResponseWriter) {
+	s.r = r
+	s.w = w
+}
+
+//StartSession ...
+func (s *Service) StartSession() error {
+	session, err := s.sessionStore.Get(s.r, StorageSessionName)
+	if err != nil {
+		return err
+	}
+	s.session = session
+	return nil
+}
+
+//GetUserSession ...
+func (s *Service) GetUserSession() (*UserSession, error) {
+	if s.session == nil {
+		return nil, ErrSessionNotStarted
+	}
+	userSession, ok := s.session.Values[UserSessionKey].(*UserSession)
+	if !ok {
+		return nil, errors.New("User session type assertion error")
+	}
+	return userSession, nil
+}
+
+//SetUserSession ...
+func (s *Service) SetUserSession(userSession *UserSession) error {
+	if s.session == nil {
+		return ErrSessionNotStarted
+	}
+	s.session.Values[UserSessionKey] = userSession
+	return s.session.Save(s.r, s.w)
+}
+
+//ClearUserSession ...
+func (s *Service) ClearUserSession() error {
+	if s.session == nil {
+		return ErrSessionNotStarted
+	}
+	delete(s.session.Values, UserSessionKey)
+	return s.session.Save(s.r, s.w)
+}
+
+//SetFlashMessage ..
+func (s *Service) SetFlashMessage(msg string) error {
+	if s.session == nil {
+		return ErrSessionNotStarted
+	}
+	s.session.AddFlash(msg)
+	return s.session.Save(s.r, s.w)
+}
+
+//GetFlashMessage ...
+func (s *Service) GetFlashMessage() (interface{}, error) {
+	if s.session == nil {
+		return nil, ErrSessionNotStarted
+	}
+	if flashes := s.session.Flashes(); len(flashes) > 0 {
+		s.session.Save(s.r, s.w)
+		return flashes[0], nil
+	}
+	return nil, nil
+}
+
+//Close ...
+func (s *Service) Close() {
+
+}
